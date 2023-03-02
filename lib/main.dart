bee5314896a69599e5cf9b5ca0f80d74cc893f07
Andreas Monitzer <andreas@monitzer.com>
2023-03-02 02:04:03 +0100

    Font fix

--- lib/main.dart
+++ lib/main.dart
@@ -108,23 +108,19 @@ class _TermPageState extends State<TermPage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(title),
       ),
       body: ColoredBox(
         color: TerminalThemes.defaultTheme.background,
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: TerminalView(_terminal!),
+          child: TerminalView(
+            _terminal!,
+            autofocus: true,
+            textStyle: const TerminalStyle(fontFamily: 'CascadiaMono'),
+          ),
         ),
       ),
     );
